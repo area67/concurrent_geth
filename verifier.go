79402b6755280252fc77fd9c1497b849f9a93f46
sam <samuel.ervolino@gmail.com>
2019-10-26 15:22:45 -0400

    added concurent slice types and functions.

--- verifier.go
+++ verifier.go
@@ -73,8 +73,38 @@ type atomicTxnCtr struct {
 	lock sync.Mutex
 }
 
-func VerifierData(t *TransactionData) {
+type ConcurrentSlice struct {
+	sync.RWMutex
+	items []interface{}
+}
 
+// Concurrent slice item
+type ConcurrentSliceItem struct {
+	Index int
+	Value interface{}
+}
+
+func (cs *ConcurrentSlice) Append(item interface{}) {
+	cs.Lock()
+	defer cs.Unlock()
+
+	cs.items = append(cs.items, item)
+}
+
+func (cs *ConcurrentSlice) Iter() <-chan ConcurrentSliceItem {
+	c := make(chan ConcurrentSliceItem)
+
+	f := func() {
+		cs.Lock()
+		defer cs.Unlock()
+		for index, value := range cs.items {
+			c <- ConcurrentSliceItem{index, value}
+		}
+		close(c)
+	}
+	go f()
+
+	return c
 }
 
 func (m *Method) setMethod(id int, itemAddr string, itemBalance int, semantics Semantics,
