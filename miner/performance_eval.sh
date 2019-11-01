bbf3f10321bd6989e95271cc69153eb49ee9b34e
ryan <ryanmdozier@gmail.com>
2019-11-01 10:47:35 -0400

    bash: performance script modifications
    script updated to perform tests up to n threads, and specify the number of operations for each thread count to perform.

--- miner/performance_eval.sh
+++ miner/performance_eval.sh
@@ -1,3 +1,13 @@
-for ((i = 0 ; i <= 1000 ; i++)); do
-	go test -run TestCommitTransactionsPerformance
+if [ $# -lt 2 ]
+	then
+	echo "Expected ./script <max threads> <num operations>"
+	exit 1
+fi
+
+echo "Running tests from 1 to ${1} Threads n = ${2} times"
+for ((j = 1 ; j <= $1 ; j*=2)); do
+	echo "Running tests for ${j} threads"
+	for ((i = 0 ; i < $2 ; i++)); do
+		go test -run TestCommitTransactionsPerformance -args -threads=$j
+	done
 done
