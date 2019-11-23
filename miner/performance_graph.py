27d78b402af840a37f9cf79b1c2c3ece227445f0
rosscwagner <rosscwagner@gmail.com>
2019-11-23 15:31:19 -0500

    orginized verifier, included original c++ tool, data grapher needs control data

--- miner/performance_graph.py
+++ miner/performance_graph.py
@@ -27,6 +27,7 @@ average = []
 # The array which will be filled with the std dev throughput of a given threadcount.
 stdError = []
 
+
 # foreach threadcount with data
 for index in threadVals :
     #get the throughput values for a given threadcount
@@ -36,11 +37,31 @@ for index in threadVals :
     #compute the std dev
     stdError.append(row.std())
 
+# get control data
+ctlData = pd.read_csv("control.txt",sep="\t", names=['Threads','Throughput'])
+# An array of threadcount values
+ctlThreadVals = ctlData.sort_values(by=['Threads']).Threads.unique()
+# The array which will be filled with the average throughput of a given threadcount.
+ctlAverage = []
+# The array which will be filled with the std dev throughput of a given threadcount.
+ctlStdError = []
+
+# foreach threadcount with data
+for index in ctlThreadVals :
+    #get the throughput values for a given threadcount
+    row = ctlData[ctlData["Threads"] == index].Throughput
+    #compute the mean
+    ctlAverage.append(row.mean())
+    #compute the std dev
+    ctlStdError.append(row.std())
 
 # Build the barplot
 x_pos = np.arange(len(threadVals))
 fig, ax = plt.subplots()
-ax.bar(x_pos, average, yerr=stdError, align='center', width=.3 , alpha=0.5, ecolor='black', capsize=10)
+ctlLine, = ax.plot(x_pos,ctlAverage,color="red",linewidth=2.5, linestyle="--")
+
+dataBars = ax.bar(x_pos, average, yerr=stdError, align='center', width=.3 , alpha=0.5, ecolor='black', capsize=10)
+
 ax.set_ylabel('Txns / Second')
 ax.set_xlabel('Threads')
 ax.set_xticks(x_pos)
@@ -48,6 +69,6 @@ ax.set_xticklabels(threadVals)
 ax.set_title('Ethereum Transaction Processing')
 ax.yaxis.grid(True)
 fig.set_tight_layout(True)
-
+ax.legend((ctlLine,dataBars),('Control','Exp Data'))
 # Save the figure
 plt.savefig(path + graphname +'.png')
